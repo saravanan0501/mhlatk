6ea405cfeddd05dc4f2250de1c6b9e13342f2d4e
saravanan0501 <saravananitbtech@gmail.com>
2019-06-16 02:50:23 -0700

    first commit command

--- BK/app/code/Cluedy/Mhlatk/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Cluedy_Mhlatk',
-    __DIR__
-);
--- BK/app/code/Cluedy/Mhlatk/view/frontend/requirejs-config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    paths: {
-        "custom-skin": "js/custom-skin"
-    }
-}
